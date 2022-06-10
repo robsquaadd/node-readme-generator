166ebc3eab4313c5b05942ded9ce34f44892ee03
robsquaadd <102495137+robsquaadd@users.noreply.github.com>
2022-06-10 15:51:41 -0400

    modified conditional in renderLicenseSection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,12 +26,16 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  licenseLink = renderLicenseLink(license);
-  return `## License
-  
-  This project is licensed under the ${license} license.
-  The link to the license is at ${licenseLink}.
-  `;
+  if (license === "None") {
+    return "";
+  } else {
+    licenseLink = renderLicenseLink(license);
+    return `## License
+    
+    This project is licensed under the ${license} license.
+    The link to the license is at ${licenseLink}.
+    `;
+  }
 }
 
 // TODO: Create a function to generate markdown for README
